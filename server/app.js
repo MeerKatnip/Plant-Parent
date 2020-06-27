67500a109348c739b3cce11a2378fda2cacbef6d
MeerKatnip <amy.roy@icloud.com>
2020-06-27 14:27:43 -0500

    creating the registration component

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.listen(PORT, function () {
   console.log("Server is running on PORT " + PORT);
