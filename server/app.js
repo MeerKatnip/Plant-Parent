21e6345f91d32b748e3d5ed5b5b41f947ed25081
MeerKatnip <amy.roy@icloud.com>
2020-06-29 13:59:32 -0500

    refactoring components and routes in index.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const bodyParser = require("body-parser");
 const { Client } = require("pg");
 const connectionString =
   "postgres://postgres:postgres@localhost:5432/plant-parent";
-// const axios = require("axios");
 
 app.use(express.json());
 app.use(express.urlencoded());
